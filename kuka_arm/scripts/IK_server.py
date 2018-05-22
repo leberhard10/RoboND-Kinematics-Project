7e6365de3dfadc5af5c24972aa1949150d93cae9
Laura Eberhard <leberhard10@gmail.com>
2018-05-21 18:11:24 -0700

    Add forward kinematics code

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -27,21 +27,83 @@ def handle_calculate_IK(req):
     else:
 
         ### Your FK code here
-        # Create symbols
-	#
-	#
-	# Create Modified DH parameters
-	#
-	#
-	# Define Modified DH Transformation matrix
-	#
-	#
-	# Create individual transformation matrices
-	#
-	#
-	# Extract rotation matrices from the transformation matrices
-	#
-	#
+        # Create symbols for joint variables
+        q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+        d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+        a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+        alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+
+	    # Create Modified DH parameters
+        s = {alpha0:   0.0, a0: 0.0,   d1: 0.75,
+             alpha1: -pi/2, a1: 0.35,  d2: 0.0,  
+             alpha2:   0.0, a2: 1.25,  d3: 0.0,
+             alpha3: -pi/2, a3: -0.54, d4: 1.50,
+             alpha4:  pi/2, a4: 0.0,   d5: 0.0,
+             alpha5: -pi/2, a5: 0.0,   d6: 0.0,
+             alpha6:   0.0, a6: 0.0,   d7: 0.303
+             }
+             
+	    # Define Modified DH Transformation matrix
+	    T0_1 = Matrix([[             cos(q1),            -sin(q1),            0,              a0],
+                       [ sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
+                       [ sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
+                       [                   0,                   0,            0,               1]])
+        T0_1 = T0_1.subs(s)
+        T1_2 = Matrix([[             cos(q2),            -sin(q2),            0,              a1],
+                       [ sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
+                       [ sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
+                       [                   0,                   0,            0,               1]])
+        T1_2 = T1_2.subs(s)
+        T3_4 = Matrix([[             cos(q4),            -sin(q4),            0,              a3],
+                       [ sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
+                       [ sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
+                       [                   0,                   0,            0,               1]])
+        T3_4 = T3_4.subs(s)        
+        T4_5 = Matrix([[             cos(q5),            -sin(q5),            0,              a4],
+                       [ sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
+                       [ sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
+                       [                   0,                   0,            0,               1]])
+        T4_5 = T4_5.subs(s)
+        T5_6 = Matrix([[             cos(q6),            -sin(q6),            0,              a5],
+                       [ sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
+                       [ sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
+                       [                   0,                   0,            0,               1]])
+        T5_6 = T5_6.subs(s)
+        T6_G = Matrix([[             cos(q7),            -sin(q7),            0,              a6],
+                       [ sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
+                       [ sin(q7)*sin(alpha6), cos(q7)*sin(alpha6),  cos(alpha6),  cos(alpha6)*d7],
+                       [                   0,                   0,            0,               1]])
+        T6_G = T6_G.subs(s)
+        
+	    #
+	    # Create individual transformation matrices
+        Simplified_T0_2 = simplify(T0_1 * T1_2) 
+        Simplified_T0_3 = simplify(T0_2 * T2_3) 
+        Simplified_T0_4 = simplify(T0_3 * T3_4) 
+        Simplified_T0_5 = simplify(T0_4 * T4_5) 
+        Simplified_T0_6 = simplify(T0_5 * T5_6)
+        Simplified_T0_G = simplify(T0_6 * T6_7)  
+        
+	    # Extract rotation matrices from the transformation matrices
+        R_z = Matrix([[ cos(np.pi), -sin(np.pi),        0],
+                      [ sin(np.pi),  cos(np.pi),        0],
+                      [         0,            0,        1]])
+        
+        R_y = Matrix([[   cos(-np.pi/2),        0,  sin(-np.pi/2)],
+                      [               0,        1,              0],
+                      [-sin(-np.pi/2q2),        0,  cos(-np.pi/2)]])
+        
+        R_corr = simplify(R_z * R_y)
+        
+        print("T0_1 = ", T0_1.evalf(subs={q1:0, q2:0, q3:0, q4:0, q5:0, q6:0}))
+        print("T0_2 = ", T0_2.evalf(subs={q1:0, q2:0, q3:0, q4:0, q5:0, q6:0}))
+        print("T0_3 = ", T0_3.evalf(subs={q1:0, q2:0, q3:0, q4:0, q5:0, q6:0}))
+        print("T0_4 = ", T0_4.evalf(subs={q1:0, q2:0, q3:0, q4:0, q5:0, q6:0}))
+        print("T0_5 = ", T0_5.evalf(subs={q1:0, q2:0, q3:0, q4:0, q5:0, q6:0}))
+        print("T0_6 = ", T0_6.evalf(subs={q1:0, q2:0, q3:0, q4:0, q5:0, q6:0}))
+        print("T0_G = ", T0_G.evalf(subs={q1:0, q2:0, q3:0, q4:0, q5:0, q6:0}))
+        
+        T_total = simplify(T0_G * R_corr)
         ###
 
         # Initialize service response
