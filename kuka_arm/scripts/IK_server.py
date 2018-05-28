2c39a7b690920b3f8a2ab1701a5fee3f2d9d31ba
Laura Eberhard <leberhard10@gmail.com>
2018-05-27 18:05:58 -0700

    passing score using next button

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -36,7 +36,7 @@ def handle_calculate_IK(req):
         s = {alpha0:   0.0, a0: 0.0,   d1: 0.75, q1:           q1,
              alpha1: -pi/2., a1: 0.35,  d2: 0.0,  q2: -pi/2. + q2,
              alpha2:   0.0, a2: 1.25,  d3: 0.0,  q3:           q3,
-             alpha3: -pi/2., a3: -0.54, d4: 1.50, q4:          q4,
+             alpha3: -pi/2., a3: -0.054, d4: 1.5, q4:          q4,
              alpha4:  pi/2., a4: 0.0,   d5: 0.0,  q5:          q5,
              alpha5: -pi/2., a5: 0.0,   d6: 0.0,  q6:          q6,
              alpha6:   0.0, a6: 0.0,   d7: 0.303, q7:            0}
@@ -72,11 +72,6 @@ def handle_calculate_IK(req):
         Rot_z = Matrix([[ cos(y), -sin(y),        0],
                       [ sin(y),  cos(y),        0],
                       [ 0,              0,        1]])
-        
-        Rot_EE = Rot_x * Rot_y * Rot_z
-        Rot_error = Rot_z.subs(y, radians(180)) * Rot_y.subs(p, radians(-90))
-        
-        Rot_EE = Rot_EE * Rot_error
         ###
 
         # Initialize service response
@@ -99,20 +94,26 @@ def handle_calculate_IK(req):
 
             ### Your IK code here
 	        # Compensate for rotation discrepancy between DH parameters and Gazebo
+            Rot_EE = Rot_x * Rot_y * Rot_z
+            Rot_error = Rot_z.subs(y, radians(180)) * Rot_y.subs(p, radians(-90))
+            
+            Rot_EE = Rot_EE * Rot_error
+            
+            # Compensate for rotation discrepancy between DH parameters and Gazebo
             Rot_EE = Rot_EE.subs({'r': roll, 'p':pitch, 'y':yaw})
             
-            EE = Matrix([[px], [py], [px]])
+            EE = Matrix([[px], [py], [pz]])
             WC = EE -  0.303 * Rot_EE[:,2]     
                           
 	        # Calculate joint angles using Geometric IK method
             theta1 = atan2(WC[1], WC[0])    
             
             A = 1.501  #d40
-            B = sqrt(pow((sqrt(WC[0]*WC[0] + WC[1]*WC[1]) - 0.35), 2) + pow(WC[0]-0.75, 2))
+            B = sqrt(pow((sqrt(WC[0]*WC[0] + WC[1]*WC[1]) - 0.35), 2) + pow(WC[2]-0.75, 2))
             C = 1.25 #a2
 
-            a = acos((pow(B,2) + pow(C,2) - pow(A,2))/(2*B*C))
-            b = acos((pow(A,2) + pow(C,2) - pow(B,2))/(2*A*C))
+            a = acos((B*B + C*C - A*A)/(2*B*C))
+            b = acos((A*A + C*C - B*B)/(2*A*C))
 
             WCa = atan2(WC[2] - 0.75, sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
 
@@ -128,7 +129,19 @@ def handle_calculate_IK(req):
             
             theta4 = atan2(R3_6[2,2], -R3_6[0,2])    
             theta5 = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2]*R3_6[2,2]),R3_6[1,2])
-            theta6 = atan2(-R3_6[1,1], R3_6[1,0])        
+            theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+            
+            FK = T0_EE.evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4:theta4, q5:theta5, q6:theta6})
+            Calc_WC = [WC[0],WC[1],WC[2]]
+            Calc_EE = [FK[0,3], FK[1,3], FK[2,3]]
+            ee_x_e = abs(Calc_EE[0]-px)
+            ee_y_e = abs(Calc_EE[1]-py)
+            ee_z_e = abs(Calc_EE[2]-pz)
+            ee_offset = sqrt(ee_x_e**2 + ee_y_e**2 + ee_z_e**2)
+            print ("\nEnd effector error for x position is: %04.8f" % ee_x_e)
+            print ("End effector error for y position is: %04.8f" % ee_y_e)
+            print ("End effector error for z position is: %04.8f" % ee_z_e)
+            print ("Overall end effector offset is: %04.8f units \n" % ee_offset)
 
 
             # Populate response for the IK request
