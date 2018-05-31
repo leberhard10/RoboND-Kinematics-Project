9e7cf77081274dcbe4fc220c3638a398fcf4267c
Laura Eberhard <leberhard10@gmail.com>
2018-05-30 19:39:03 -0700

    Add code comment updates

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -18,6 +18,43 @@ from geometry_msgs.msg import Pose
 from mpmath import *
 from sympy import *
 
+# Create symbols for joint variables
+q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+
+# Create Modified DH parameters
+s = {alpha0:   0.0, a0: 0.0,   d1: 0.75, q1:           q1,
+     alpha1: -pi/2., a1: 0.35,  d2: 0.0,  q2: -pi/2. + q2,
+     alpha2:   0.0, a2: 1.25,  d3: 0.0,  q3:           q3,
+     alpha3: -pi/2., a3: -0.054, d4: 1.5, q4:          q4,
+     alpha4:  pi/2., a4: 0.0,   d5: 0.0,  q5:          q5,
+     alpha5: -pi/2., a5: 0.0,   d6: 0.0,  q6:          q6,
+     alpha6:   0.0, a6: 0.0,   d7: 0.303, q7:            0}
+     
+# Define Modified DH Transformation matrix
+def TF_Matrix(alpha, a, d, q):
+    TF = Matrix([[             cos(q),            -sin(q),            0,              a],
+               [ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+               [ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
+               [                   0,                   0,            0,               1]])
+    return TF
+    
+# Extract rotation matrices from the transformation matrices
+r, p, y = symbols('r p y')
+Rot_x = Matrix([[ 1,              0,        0],
+              [ 0,        cos(r), -sin(r)],
+              [ 0,        sin(r),  cos(r)]])
+
+Rot_y = Matrix([[ cos(p),        0,  sin(p)],
+              [       0,        1,        0],
+              [-sin(p),        0,  cos(p)]])
+
+Rot_z = Matrix([[ cos(y), -sin(y),        0],
+              [ sin(y),  cos(y),        0],
+              [ 0,              0,        1]])
+
 
 def handle_calculate_IK(req):
     rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
@@ -25,30 +62,7 @@ def handle_calculate_IK(req):
         print "No valid poses received"
         return -1
     else:
-
-        # Create symbols for joint variables
-        q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
-        d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
-        a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
-        alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
-
-        # Create Modified DH parameters
-        s = {alpha0:   0.0, a0: 0.0,   d1: 0.75, q1:           q1,
-             alpha1: -pi/2., a1: 0.35,  d2: 0.0,  q2: -pi/2. + q2,
-             alpha2:   0.0, a2: 1.25,  d3: 0.0,  q3:           q3,
-             alpha3: -pi/2., a3: -0.054, d4: 1.5, q4:          q4,
-             alpha4:  pi/2., a4: 0.0,   d5: 0.0,  q5:          q5,
-             alpha5: -pi/2., a5: 0.0,   d6: 0.0,  q6:          q6,
-             alpha6:   0.0, a6: 0.0,   d7: 0.303, q7:            0}
-             
-        # Define Modified DH Transformation matrix
-        def TF_Matrix(alpha, a, d, q):
-            TF = Matrix([[             cos(q),            -sin(q),            0,              a],
-                       [ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
-                       [ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
-                       [                   0,                   0,            0,               1]])
-            return TF
-                       
+         
         T0_1 = TF_Matrix(alpha0, a0, d1, q1).subs(s)
         T1_2 = TF_Matrix(alpha1, a1, d2, q2).subs(s)
         T2_3 = TF_Matrix(alpha2, a2, d3, q3).subs(s)
@@ -58,20 +72,6 @@ def handle_calculate_IK(req):
         T6_EE = TF_Matrix(alpha6, a6, d7, q7).subs(s)
         
         T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
-        
-	    # Extract rotation matrices from the transformation matrices
-        r, p, y = symbols('r p y')
-        Rot_x = Matrix([[ 1,              0,        0],
-                      [ 0,        cos(r), -sin(r)],
-                      [ 0,        sin(r),  cos(r)]])
-
-        Rot_y = Matrix([[ cos(p),        0,  sin(p)],
-                      [       0,        1,        0],
-                      [-sin(p),        0,  cos(p)]])
-
-        Rot_z = Matrix([[ cos(y), -sin(y),        0],
-                      [ sin(y),  cos(y),        0],
-                      [ 0,              0,        1]])
         ###
 
         # Initialize service response
@@ -106,7 +106,7 @@ def handle_calculate_IK(req):
             WC = EE -  0.303 * Rot_EE[:,2]     
                           
 	        # Calculate joint angles using Geometric IK method
-            theta1 = atan2(WC[1], WC[0])    
+            theta1 = atan2(WC[1], WC[0]) #WC[1] = y & WC[0] = x    
             
             A = 1.501  #d40
             B = sqrt(pow((sqrt(WC[0]*WC[0] + WC[1]*WC[1]) - 0.35), 2) + pow(WC[2]-0.75, 2))
@@ -129,7 +129,7 @@ def handle_calculate_IK(req):
             
             theta4 = atan2(R3_6[2,2], -R3_6[0,2])    
             theta5 = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2]*R3_6[2,2]),R3_6[1,2])
-            theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+            theta6 = atan2(-R3_6[1,1], R3_6[1,0])  
             
             FK = T0_EE.evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4:theta4, q5:theta5, q6:theta6})
             Calc_WC = [WC[0],WC[1],WC[2]]
